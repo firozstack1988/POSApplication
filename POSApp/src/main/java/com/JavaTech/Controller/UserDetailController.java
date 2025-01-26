aa26e8da3b3b4ac66e8415073dbd0126d42d99b6
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2025-01-26 16:35:54 +0600

    Add files via upload

--- POSApp/src/main/java/com/JavaTech/Controller/UserDetailController.java
+++ POSApp/src/main/java/com/JavaTech/Controller/UserDetailController.java
@@ -3,29 +3,33 @@ package com.JavaTech.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.JavaTech.Dto.ResponseModel;
 import com.JavaTech.Dto.UserRequest;
-import com.JavaTech.Entity.CustomerInfo;
 import com.JavaTech.Entity.UserDetail;
-import com.JavaTech.Repository.UserDetailRepo;
+import com.JavaTech.Service.UserDetailServiceImpl;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
 public class UserDetailController {
-    @Autowired
-	private UserDetailRepo userDetailRepo;
+	 @Autowired
+	 private UserDetailServiceImpl userDetailService;
 	
 	@PostMapping("/add")
-	public ResponseEntity<UserDetail> save(@RequestBody @Valid UserRequest userRequest) {
+	public ResponseEntity<String> add(@RequestBody @Valid UserRequest userRequest) {
 		UserDetail userDetail= UserDetail.build(null, userRequest.getUserName(), userRequest.getEmail(), userRequest.getMobile(), userRequest.getNationality());
-		return new ResponseEntity<>(userDetailRepo.save(userDetail),HttpStatus.CREATED) ;
+		return new ResponseEntity<>(userDetailService.add(userDetail),HttpStatus.CREATED) ;	
+	}
+	
+	@GetMapping("/findUser/{id}")
+	public ResponseEntity<UserDetail> findUser(@PathVariable("id") Long id) {
+		return new ResponseEntity<>(userDetailService.findById(id),HttpStatus.FOUND) ;	
 	}
 }
