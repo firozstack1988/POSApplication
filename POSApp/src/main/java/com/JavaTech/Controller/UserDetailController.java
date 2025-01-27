4999e21ca48113433ff2a5e12292af7b5effe79e
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2025-01-27 12:53:36 +0600

    Add files via upload

--- POSApp/src/main/java/com/JavaTech/Controller/UserDetailController.java
+++ POSApp/src/main/java/com/JavaTech/Controller/UserDetailController.java
@@ -1,11 +1,11 @@
 package com.JavaTech.Controller;
 
-import java.util.Optional;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.JavaTech.Dto.CustomerWrapper;
 import com.JavaTech.Dto.UserRequest;
-import com.JavaTech.Entity.CustomerInfo;
 import com.JavaTech.Entity.UserDetail;
 import com.JavaTech.Service.UserDetailServiceImpl;
 
@@ -30,14 +28,25 @@ public class UserDetailController {
 	 private UserDetailServiceImpl userDetailService;
 	 @Autowired
 	 private ModelMapper modelMapper;
+	 @Autowired
+	 private PasswordEncoder passwordEncoder;
+	 
+	 @GetMapping("/welcome")
+	 public String findUser() {
+		 return "welcome page";	
+		}
 	
 	@PostMapping("/add")
+	//@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public ResponseEntity<String> add(@RequestBody @Valid UserRequest userRequest) {
-		UserDetail userDetail= UserDetail.build(null, userRequest.getUserName(), userRequest.getEmail(), userRequest.getMobile(), userRequest.getNationality());
+		UserDetail userDetail= UserDetail.build(null, userRequest.getUserName(),passwordEncoder.encode(userRequest.getPassword()), 
+				 userRequest.getEmail(), userRequest.getMobile(), userRequest.getNationality(),userRequest.getUserRole());
 		return new ResponseEntity<>(userDetailService.add(userDetail),HttpStatus.CREATED) ;	
 	}
 	
 	@GetMapping("/findUser/{id}")
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public ResponseEntity<UserDetail> findUser(@PathVariable("id") Long id) {
 		return new ResponseEntity<>(userDetailService.findById(id),HttpStatus.FOUND) ;	
 	}
@@ -45,8 +54,7 @@ public class UserDetailController {
 	@PatchMapping("modify/{id}")
 	public ResponseEntity<String> partialUpdateUser(@PathVariable("id") Long id,@RequestBody UserRequest userRequest) {
 		UserDetail userDetail=modelMapper.map(userRequest, UserDetail.class);
-		return new ResponseEntity<>(userDetailService.partialUpdateUser(id,userDetail),HttpStatus.CREATED);
-		
+		return new ResponseEntity<>(userDetailService.partialUpdateUser(id,userDetail),HttpStatus.CREATED);		
 	}
 	
 	@PutMapping("/modify/{id}")
