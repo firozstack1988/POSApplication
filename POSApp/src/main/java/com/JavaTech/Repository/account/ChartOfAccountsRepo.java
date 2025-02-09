1c6685d3372dba4cd97af19d1f781e56f1ba4e86
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2025-02-10 01:03:53 +0600

    Add files via upload

--- POSApp/src/main/java/com/JavaTech/Repository/account/ChartOfAccountsRepo.java
+++ POSApp/src/main/java/com/JavaTech/Repository/account/ChartOfAccountsRepo.java
@@ -1,6 +1,9 @@
 package com.JavaTech.Repository.account;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +13,11 @@ import com.JavaTech.Entity.accounts.ChartOfAccounts;
 public interface ChartOfAccountsRepo extends JpaRepository<ChartOfAccounts,Long>{
 	
 	public ChartOfAccounts findByAccountGLCode(@Param("accountGLCode") String accountGLCode);
+	
+	@Query("SELECT t FROM ChartOfAccounts t where t.isAccountForPurchase=:isAccountForPurchase and t.isAccountActive=:isAccountActive ")
+	public ChartOfAccounts findChartOfAccountByAccountForPurchase(@Param("isAccountForPurchase") boolean isAccountForPurchase,@Param("isAccountActive") boolean isAccountActive);
+	
+	public Optional<ChartOfAccounts> findByIsAccountForPurchaseAndIsAccountActive(@Param("isAccountForPurchase") boolean isAccountForPurchase,@Param("isAccountForPurchase") boolean isAccountActive);
+	public Optional<ChartOfAccounts> findByIsAccountForCashInHandAndIsAccountActive(@Param("isAccountForCashInHand") boolean isAccountForCashInHand,@Param("isAccountActive") boolean isAccountActive);
 
 }
--- POSApp/src/main/java/com/JavaTech/Repository/account/PaymentVoucherRepo.java
+++ POSApp/src/main/java/com/JavaTech/Repository/account/PaymentVoucherRepo.java
@@ -1,10 +1,20 @@
 package com.JavaTech.Repository.account;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.JavaTech.Entity.accounts.PaymentVoucher;
 
 @Repository
 public interface PaymentVoucherRepo extends JpaRepository<PaymentVoucher,Long>{
+ 
+	@Query("Select max(t.voucherNum) from PaymentVoucher t where t.tranDate=:currDate")
+    public Optional<Long> findMaxVoucherNo(@Param("currDate") Date currDate);
+	
+	 public Optional<PaymentVoucher> findTopVoucherNoByTranDate(@Param("currDate") Date currDate);
 
 }
